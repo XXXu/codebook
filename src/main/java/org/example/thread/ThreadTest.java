35cda525d07dd9a8fbeff31803868f099e481424
xuxiaoxiao <xiaoxiao.xu@transwarp.io>
2022-02-28 19:47:34 +0800

    thread

--- src/main/java/org/example/thread/ThreadTest.java
+++ src/main/java/org/example/thread/ThreadTest.java
@@ -1,19 +1,80 @@
 package org.example.thread;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ThreadTest {
+
     public static void main(String[] args) {
-        Object object = new Object();
+        /*Object object = new Object();
         Thread2 thread2 = new Thread2(object);
         thread2.setName("thread2");
 
         Thread1 thread1 = new Thread1(object);
         thread1.setName("thread1");
         thread1.start();
-        thread2.start();
+        thread2.start();*/
+        AtomicBoolean flag = new AtomicBoolean(false);
+        AtomicInteger num = new AtomicInteger(1);
+
+        Thread threada = new ThreadA(flag, num);
+        threada.setName("a");
+
+        Thread threadb = new ThreadB(flag, num);
+        threadb.setName("b");
+
+        threada.start();
+        threadb.start();
 
     }
 
-    static class Thread2 extends Thread {
+    static class ThreadA extends Thread {
+        private AtomicBoolean flag;
+        private AtomicInteger num;
+
+        public ThreadA(AtomicBoolean flag, AtomicInteger num) {
+            this.num = num;
+            this.flag = flag;
+        }
+
+        @Override
+        public void run() {
+            while (num.get() < 100) {
+                if (!flag.get()) {
+                    System.out.println(Thread.currentThread().getName() + ": " + num);
+                    num.incrementAndGet();
+                    flag.set(true);
+                }
+            }
+
+        }
+    }
+
+    static class ThreadB extends Thread {
+        private AtomicBoolean flag;
+        private AtomicInteger num;
+
+        public ThreadB(AtomicBoolean flag, AtomicInteger num) {
+            this.num = num;
+            this.flag = flag;
+        }
+
+        @Override
+        public void run() {
+            while (num.get() < 100) {
+                if (flag.get()) {
+                    System.out.println(Thread.currentThread().getName() + ": " + num);
+                    num.incrementAndGet();
+                    flag.set(false);
+                }
+            }
+
+        }
+    }
+
+
+
+    /*static class Thread2 extends Thread {
         private Object object;
 
         public Thread2(Object object) {
@@ -57,7 +118,7 @@ public class ThreadTest {
             }
 
         }
-    }
+    }*/
 }
 
 
