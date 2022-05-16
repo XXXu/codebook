285e217ec6ca239941f25650182f00d84f0bea4e
xuxiaoxiao <xiaoxiao.xu@transwarp.io>
2022-05-16 11:07:49 +0800

    test

--- src/main/java/org/example/test/Test.java
+++ src/main/java/org/example/test/Test.java
@@ -1,67 +1,69 @@
 package org.example.test;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Test {
-    static  class Directory {
-        private int parentid;
-        private List<Integer> inodeid;
-
-        public int getParentid() {
-            return parentid;
+    static  class Directory implements Runnable{
+        private static volatile AtomicInteger atomicInteger = new AtomicInteger(0);
+        private static volatile int i=0;
+        public void atomicI() {
+            atomicInteger.addAndGet(1);
         }
 
-        public void setParentid(int parentid) {
-            this.parentid = parentid;
-        }
-
-        public List<Integer> getInodeid() {
-            return inodeid;
-        }
-
-        public void setInodeid(List<Integer> inodeid) {
-            this.inodeid = inodeid;
+        public void setI() {
+            i++;
         }
 
         @Override
-        public String toString() {
-            return "Directory{" +
-                    "parentid=" + parentid +
-                    ", inodeid=" + inodeid +
-                    '}';
+        public void run() {
+//            atomicI();
+            setI();
+        }
+
+        public int getAtomic() {
+            return atomicInteger.get();
+        }
+
+        public int getI() {
+            return i;
         }
     }
 
     public static void main(String[] args) throws IOException {
-        /*BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("/root/workspace/transwarp/tdfs/target/debug/asdasd.txt")));
-        String line = null;
-        Map<Integer, Directory> map = new HashMap<>();
-        while (!((line = bufferedReader.readLine()) == null)) {
-            String[] split = line.split(" ");
-            int key = Integer.parseInt(split[0].substring(1, split[0].length() - 1));
-            int value = Integer.parseInt(split[1]);
+        /*Directory directory = new Directory();
+        int count = 0;
+        for (int i = 0; i < 10000; i++) {
+            count++;
+            Thread thread = new Thread(directory);
+            thread.start();
+        }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("count is: "+count +", directory is :"+directory.getAtomic() +", i is: "+directory.getI());*/
 
-            if (map.get(value) == null) {
-                Directory directory = new Directory();
-                directory.setParentid(value);
-                List<Integer> integerList = new ArrayList<>();
-                integerList.add(key);
-                directory.setInodeid(integerList);
-                map.put(value, directory);
-            } else {
-                Directory directory = map.get(value);
-                directory.getInodeid().add(key);
-            }
-        }
-        for (Map.Entry entry : map.entrySet()) {
-            System.out.println(entry.getValue());
-            System.out.println("========");
-        }
-        bufferedReader.close();*/
-        System.out.println(2 << 14 - 1);
+//        String str = "18446744073709551615";
+//        System.out.println("max: "+Long.MAX_VALUE);
+//        System.out.println("str: "+str);
+//        System.out.println(Long.valueOf(str));
+
+        Configuration conf = new Configuration();
+        conf.addResource(new Path("./conf/core-site.xml"));
+        conf.addResource(new Path("./conf/hdfs-site.xml"));
+        conf.set("dfs.ha.automatic-failover.enabled.nameservice1", "true");
+
+        boolean aBoolean = conf.getBoolean("dfs.ha.automatic-failover.enabled", false);
+        System.out.println("automatic is: " + aBoolean);
+
     }
 }
